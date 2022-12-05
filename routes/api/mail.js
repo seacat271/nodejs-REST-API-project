8fcb237e606dbcb36fc3a7bec3e6831736106dd3
seacat271 <seacat271083@gmail.com>
2022-12-05 15:32:54 +0200

    add mailer

---
+++ routes/api/mail.js
@@ -0,0 +1,44 @@
+const sgMail = require('@sendgrid/mail');
+const express = require('express')
+const router = express.Router();
+
+const config = {
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'kirill.art3m3nko@gmail.com',
+        pass: process.env.PASSWORD,
+    }
+}
+
+
+    router.get('/api/mail', (req, res, next) => {
+        res.render("index")
+    }) 
+
+    router.post('/send-sendgrid', (req, res, next) => {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+          to: req.body.email,
+          from: 'kirill.art3m3nko@gmail.com',
+          subject: 'test test',
+          text: req.body.text,
+        //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+        };
+        sgMail
+        .send(msg)
+        .then(() => {
+            console.log("E-mail send")
+        }, error => {
+          console.log(error);
+      
+          if (error.response) {
+            console.error(error.response.body)
+          }
+        }).finally(()=> {
+            res.redirect('/api/mail')
+        });
+    })
+
+module.exports = router;
